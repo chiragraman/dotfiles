ee0a5431b14e882bb981bd457306286ce98f9a73
Chirag Raman <chirag@production.pro>
2016-01-23 19:11:38 -0500

    move completions to completion.zsh

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -8,8 +8,14 @@ _force_rehash() {
 # forces zsh to realize new commands
 zstyle ':completion:*' completer _oldlist _expand _force_rehash _complete _match
 
+# more completers to use
+zstyle ':completion:*' _ignored _approximate
+
 # matches case insensitive for lowercase
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# menu if nb items > 2
+zstyle ':completion:*' menu select=2
