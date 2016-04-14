392296042c8a3fa98c4b3b6d95afcef784cd3e9b
Chirag Raman <chirag@production.pro>
2016-04-13 23:09:26 -0400

    Cleanup grc.zsh

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # GRC colorizes nifty unix tools all over the place
-# shellcheck disable=2039
-if grc &>/dev/null && ! brew &>/dev/null; then
-  . "$(brew --prefix)/etc/grc.bashrc"
+if (( $+commands[grc] )) && (( $+commands[brew] )); then
+  source `brew --prefix`/etc/grc.bashrc
 fi
