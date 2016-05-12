6769e75de85a3a75d57668c4a6a32a21aa4fa63d
Chirag Raman <chirag.raman@gmail.com>
2016-05-12 11:03:20 -0400

    Add atom-material-syntax-dark to install.sh

--- atom.symlink/install.sh
+++ atom.symlink/install.sh
@@ -4,7 +4,7 @@ apm install \
     atom-beautify \
     pdf-view zen markdown-preview-plus markdown-writer wordcount \
     minimap highlight-selected minimap-highlight-selected \
-    atom-material-ui atom-material-syntax file-icons \
+    atom-material-ui atom-material-syntax atom-material-syntax-dark file-icons \
     tool-bar tool-bar-main \
     docblockr linter \
     autocomplete-clang linter-clang switch-header-source \
