be1e7a5fa62b1c37460b573c64df8c59b2262b58
Chirag Raman <chirag.raman@gmail.com>
2017-01-13 13:22:13 -0500

    Add virtual environment configuration

---
+++ python/virtual_env.zsh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/Projects
+export VIRTUALENVWRAPPER_PYTHON=`which python`
+export VIRTUALENVWRAPPER_VIRTUALENV=`which virtualenv`
+source `which virtualenvwrapper.sh`
