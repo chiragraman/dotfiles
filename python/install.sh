81d648b2b8ec13d75acca1b4d20ed8d9b5a97e3d
chiragraman <chirag.raman@gmail.com>
2017-03-24 22:48:55 -0400

    Migrate to conda
    Remove homebrew python, virtualenv, and virtualenvwrapper

--- python/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-cd "$(dirname "$0")"
-
-pip install -r requirements.txt
--- python/virtual_env.zsh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-export WORKON_HOME=$HOME/.virtualenvs
-export PROJECT_HOME=$HOME/Projects
-export VIRTUALENVWRAPPER_PYTHON=`which python`
-export VIRTUALENVWRAPPER_VIRTUALENV=`which virtualenv`
-source `which virtualenvwrapper.sh`
