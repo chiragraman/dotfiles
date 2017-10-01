ae139c33dcf65c8383f914e15732ff02df8ede3e
Chirag Raman <chirag.raman@gmail.com>
2017-10-01 13:52:46 -0400

    Add vscode install.sh and settings

---
+++ vscode/install.sh
@@ -0,0 +1,30 @@
+#!/bin/zsh
+
+# Find path for User and Workspace Settings
+if [ "$(uname -s)" = "Darwin" ]; then
+  CONFIG_ROOT="$HOME"/Library/Application\ Support/Code/User
+elif [ "$(uname -s)" = "Linux" ]; then
+  CONFIG_ROOT="$HOME"/.config/Code/User
+fi
+
+# Create links for settings, keybindings, snippets
+ln -sf "$DOTFILES"/vscode/settings.json "$CONFIG_ROOT"/settings.json
+ln -sf "$DOTFILES"/vscode/keybindings.json "$CONFIG_ROOT"/keybindings.json
+ln -sf "$DOTFILES"/vscode/snippets/ "$CONFIG_ROOT"/snippets
+
+# Install extensions
+EXTENSIONS=(
+  $(sed "s/[[:space:]]*#.*//;/^[[:space:]]*$/d" extensions.txt)
+)
+
+for VARIANT in "code" \
+               "code-insiders"
+do
+  if hash $VARIANT 2>/dev/null; then
+    echo "› Installing extensions for $VARIANT"
+    for EXTENSION in ${EXTENSIONS[@]}
+    do
+      $VARIANT --install-extension $EXTENSION
+    done
+  fi
+done
