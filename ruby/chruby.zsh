a808be6c2b9e8f33b6e846ec3b417caacf5f218f
Chirag Raman <chirag.raman@gmail.com>
2023-03-04 19:08:07 +0100

    Add chruby config

---
+++ ruby/chruby.zsh
@@ -0,0 +1,11 @@
+#!/bin/sh
+###
+# File: chruby.zsh
+# Created Date: Wednesday, February 1st 2023, 1:20:04 am
+# Author: Chirag Raman
+#
+# Copyright (c) 2023 Chirag Raman
+###
+
+source $(brew --prefix)/opt/chruby/share/chruby/chruby.sh
+source $(brew --prefix)/opt/chruby/share/chruby/auto.sh
