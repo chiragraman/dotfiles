197e6fac80bd108c0e1906cd5b97109c5fdb20f3
Chirag Raman <chirag.raman@gmail.com>
2023-12-28 00:32:07 +0100

    Refactor fonts installation using latest fira code release

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
-URL="https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/FiraCode.zip"
+
+# Get the URL of the zip file with "Fira_Code" in the file name from amongst the release assets
+zip_url=$(curl --silent "https://api.github.com/repos/tonsky/FiraCode/releases/latest" | \
+    jq -r '.assets[] | select(.name | contains("Fira_Code")) | .browser_download_url' | sed 's/\\//g')
 
 install() {
-	curl -L -s -o /tmp/fira.zip "$URL"
+	curl -L -s -o /tmp/fira.zip "$zip_url"
 	unzip /tmp/fira.zip -d /tmp/FiraCode
 	cp /tmp/FiraCode/*.ttf "$1"
 }
 
 if [ "$(uname -s)" = "Linux" ]; then
     sudo apt install fonts-firacode
-if [ "$(uname -s)" = "Darwin" ]; then
-	if which brew >/dev/null 2>&1; then
-		brew cask install font-firacode-nerd-font
-		brew cask install font-firacode-nerd-font-mono
-	else
+elif [ "$(uname -s)" = "Darwin" ]; then
 		install ~/Library/Fonts
-	fi
 fi
