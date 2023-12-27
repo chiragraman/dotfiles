a2f29c6ac972cae1fa2bea916d07618b131854c9
Chirag Raman <chirag.raman@gmail.com>
2023-12-27 19:22:38 +0100

    Add newline before echo

--- osx/install.sh
+++ osx/install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 [ "$(uname -s)" != "Darwin" ] && exit 0
-echo "› sudo softwareupdate -ia"
+echo "\n› sudo softwareupdate -ia"
 sudo softwareupdate -ia
--- vscode/install.sh
+++ vscode/install.sh
@@ -21,7 +21,7 @@ for VARIANT in "code" \
                "code-insiders"
 do
   if hash $VARIANT 2>/dev/null; then
-    echo "› Installing extensions for $VARIANT"
+    echo "\n› Installing extensions for $VARIANT"
     for EXTENSION in ${EXTENSIONS[@]}
     do
       $VARIANT --install-extension $EXTENSION
