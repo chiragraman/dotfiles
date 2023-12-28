f670c3c0fc5dace86adbfa851d58978c0dcafca1
Chirag Raman <chirag.raman@gmail.com>
2023-12-28 19:35:12 +0100

    Add commands for shellenv to .zprofile for homebrew in the install script

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -6,6 +6,14 @@
 # using Homebrew.
 
 # Check for Homebrew and Install
-if test "$(uname)" = "Darwin" -a ! "$(which brew)"; then
+if test "$(uname -s)" = "Darwin" -a ! "$(which brew)"; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Add the command to set variables to .zprofile
+if [ -f ~/.zprofile ] && grep -q 'eval "\$(/opt/homebrew/bin/brew shellenv)"' ~/.zprofile; then
+    echo "The command for brew shellenv is already present in ~/.zprofile"
+else
+    printf '%s\n' '# Sets various variables and adds them to path' 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+    echo "The command for brew shellenv has been added to ~/.zprofile"
 fi
